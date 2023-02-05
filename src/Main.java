077832b2a244f344f6695a539f74ab82c5428caf
Travis Feldkamp <feldkatr@mail.uc.edu>
2023-02-05 14:10:26 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOfKids = 6;
+        String isItRaining = "yes";
+        double priceOfGas = 3.50;
+        double favoriteNumber = 34;
+        double shoeSize = 9.5;
+        int birthMonth = 8;
+        String fullName = "Travis Ryan Feldkamp";
+        System.out.println("The number of kids in my family is " + numberOfKids);
+
+
+
+    }
+}
